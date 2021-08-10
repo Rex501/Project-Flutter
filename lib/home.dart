b07e3f92b45d02ce4c3ec77acff7c6ab61d1d357
Jhonatan <phaton777@hotmail.com>
2021-08-10 14:47:57 -0400

    clase 2 (03/08/2021)

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_places/description_place.dart';
 import 'package:proyecto_places/review.dart';
+import 'package:proyecto_places/review_list.dart';
 
 class MyHome extends StatelessWidget
 {
@@ -8,44 +9,53 @@ class MyHome extends StatelessWidget
   Widget build(BuildContext context)
   {
     final descriptionPlace = Container
-    (
-      margin: EdgeInsets.only
       (
-        top: 250,
-        left: 30,
-        right: 30
+      margin: EdgeInsets.only
+        (
+          top: 50,
+          left: 30,
+          right: 30
       ),
       child: DescriptionPlace("Uyuni", 4, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
     );
 
-    final review = Container
+    final reviewList = Container
       (
-      margin: EdgeInsets.only
-        (
-          top: 300,
-          left: 30,
-          right: 30
-      ),
-      height: 80,
+        margin: EdgeInsets.only
+          (
+            top: 20,
+            left: 30,
+            right: 30
+        ),
 
-      child: Review("assets/images/persona1.jpg","Laura Leon", "1 reviews - 4 photos", 2, "Muy buen lugar para visitar ")
+        child: ReviewList ()
+    );
+
+    //listView
+    final listView = ListView
+      (
+      children: <Widget>
+      [
+        descriptionPlace,
+        reviewList
+      ],
     );
 
 
     return Scaffold
-    (
-      appBar: AppBar
       (
-        title: Text
+      appBar: AppBar
         (
-          "My Places"
+        title: Text
+          (
+            "My Places"
         ),
       ),
       body: Stack
-      (
+        (
         children: <Widget>
         [
-          review
+          listView
         ],
       ),
     );
---
+++ lib/review_list.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_places/review.dart';
+
+class ReviewList extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    final reviewlist = Column
+      (
+      children: <Widget>
+      [
+        Review ("assets/images/persona1.jpg","Laura Leon", "1 reviews - 3 photos", 4, "Muy buen lugar para visitar "),
+        Review ("assets/images/persona2.jpg","Maria Eugenia", "4 reviews - 2 photos", 3, "Nunca visite el lugar "),
+        Review ("assets/images/persona3.jpg","Lorena", "3 reviews - 2 photos", 5, "Lo recomiendo bastante "),
+        Review ("assets/images/persona4.jpg","Laura Leon", "1 reviews - 4 photos", 2, "Muy buen lugar para visitar "),
+        Review ("assets/images/persona5.jpg","Carlos", "3 reviews - 4 photos", 3, "Hermoso lugar para visitar")
+      ],
+    );
+
+    return reviewlist;
+  }
+}
